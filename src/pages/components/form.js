94aa9bb32069f92e7eb4ad5431abac0414c2d659
Monnus <maun2velsket@gmail.com>
2022-07-07 08:51:27 +0200

    second commit added landingpage

--- src/pages/components/form.js
+++ src/pages/components/form.js
@@ -10,7 +10,7 @@ import { ToastContainer, toast } from 'react-toastify';
 
 
 
-function InsideForm({discription,title,setPassword, setEmail,handleAction,redirectBtn ,image}){
+function InsideForm({discription,title,setPassword, setEmail,handleAction,redirectBtn}){
 
         return (
     
@@ -26,7 +26,7 @@ function InsideForm({discription,title,setPassword, setEmail,handleAction,redire
                 sx={{  '& > :not(style)': { m: 1, width: '25ch' }, }} noValidate autoComplete="off">
                 <TextField id="email" label="Enter the Email" variant="outlined" onChange={(e) => setEmail(e.target.value)} />
                     <TextField id="password" label="Enter the Password" variant="outlined"  onChange={(e) => setPassword(e.target.value)}/>
-                    <input type="file" style={{outline:"none", border:"none",cursor:"pointer",color:"rgba(0,0,0,0)"}} />
+                    
             </Box>
             <Button Button variant="contained" href="#contained-buttons"  onClick={handleAction} >{title}</Button>
      <p>{discription}have an account then  don't you {title} <Button variant="outlined" href="#outlined-buttons" onClick={redirectBtn}>
